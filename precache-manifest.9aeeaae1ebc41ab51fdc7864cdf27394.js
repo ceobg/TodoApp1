02e6313ba58dbef21880b34b104c4fdd38bfea08
Shona Okereke <muaijebelle@outlook.com>
2020-10-19 13:35:24 -0500

    Updates

---
+++ precache-manifest.9aeeaae1ebc41ab51fdc7864cdf27394.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155064a14e7d986509f6a16bf6579647",
+    "url": "/TodoApp1/index.html"
+  },
+  {
+    "revision": "13c9f6beebf44ff60a53",
+    "url": "/TodoApp1/static/css/2.cfed7de4.chunk.css"
+  },
+  {
+    "revision": "059315c4ea5e40f395b0",
+    "url": "/TodoApp1/static/css/main.a842ecf2.chunk.css"
+  },
+  {
+    "revision": "13c9f6beebf44ff60a53",
+    "url": "/TodoApp1/static/js/2.98165599.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TodoApp1/static/js/2.98165599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059315c4ea5e40f395b0",
+    "url": "/TodoApp1/static/js/main.7f239be0.chunk.js"
+  },
+  {
+    "revision": "9576a073c52f9b0ff398",
+    "url": "/TodoApp1/static/js/runtime-main.9d4a9002.js"
+  },
+  {
+    "revision": "7740c5a0e87a61cbb795c4e45edd00ea",
+    "url": "/TodoApp1/static/media/Fuego.7740c5a0.mp4"
+  },
+  {
+    "revision": "8fecdd83503ebda712e8210bcaea0621",
+    "url": "/TodoApp1/static/media/Stars.8fecdd83.mov"
+  },
+  {
+    "revision": "5195ad3d1acdc6d671cd634c89141a34",
+    "url": "/TodoApp1/static/media/goldd.5195ad3d.png"
+  }
+]);
